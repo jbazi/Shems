cfadbaa72881add1cee0119bce1494c2085bec5d
jbazi <bazi.the.developer@gmail.com>
2016-08-30 17:04:03 -0400

    more changes

--- MyPortfolio/js/shems.js
+++ MyPortfolio/js/shems.js
@@ -10,5 +10,46 @@
 
 $(window).on('load', function () {
     $(".loader").fadeOut("slow");
-})
+    $(function () {
+        var fileName = "Mudassar_Khan.pdf";
+        $("#btnShow").click(function () {
+            $("#dialog").dialog({
+                modal: true,
+                title: fileName,
+                width: 540,
+                height: 450,
+                buttons: {
+                    Close: function () {
+                        $(this).dialog('close');
+                    }
+                },
+                open: function () {
+                    var object = "<object data=\"{FileName}\" type=\"application/pdf\" width=\"500px\" height=\"300px\">";
+                    object += "If you are unable to view file, you can download from <a href = \"{FileName}\">here</a>";
+                    object += " or download <a target = \"_blank\" href = \"http://get.adobe.com/reader/\">Adobe PDF Reader</a> to view the file.";
+                    object += "</object>";
+                    object = object.replace(/{FileName}/g, "Files/" + fileName);
+                    $("#dialog").html(object);
+                }
+            });
+        });
+    });
+});
+
+function setContentFontSize() {
+    document.getElementById("textWrap").style.fontSize = (12 + Math.max(0, (document.getElementById("footerImage").width - 620) / 55)) + "px";
+}
+
+var deferResizeHandler = false;
+
+function resizeHandler() {
+    if (!deferResizeHandler) {
+        setContentFontSize();
+        deferResizeHandler = true;
+        window.setTimeout(function () { deferResizeHandler = false; setContentFontSize(); }, 300);
+    }
+}
+
+window.addEventListener("resize", resizeHandler);
+window.addEventListener("load", setContentFontSize);
 
